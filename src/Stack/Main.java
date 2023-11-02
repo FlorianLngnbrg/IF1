8d46a477ee7061a2b288e28b99ede784967b79fc
FlorianLngnbrg <florian.langenberg@urs-os.de>
2023-11-02 09:00:34 +0100

    ...

---
+++ src/Stack/Main.java
@@ -0,0 +1,13 @@
+package Stack;
+
+public class Main {
+
+    public static  void main(String [] args)
+    {
+        Verschiebebahnhof bhf = new Verschiebebahnhof();
+
+        bhf.sort
+        System.out.println(bhf);ieren();
+        System.out.println(bhf);
+    }
+}
---
+++ src/Stack/intKeller.java
@@ -0,0 +1,16 @@
+package Stack;
+public interface intKeller {
+
+    // liefert true, falls Keller leer, sonst false
+    public boolean isEmpty();
+
+    // legt neues Element x auf den Keller
+    public void push(int x);
+
+    // liefert das oberste Kellerelement
+    public int peek();
+
+    // löscht das oberste Kellerelement und liefert es zurück
+    public int pop();
+
+}
