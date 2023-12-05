6e0bfa159da0b9b01f1d2106e3c4822bb60bf54c
FlorianLngnbrg <florian.langenberg@outlook.com>
2023-12-05 19:22:25 +0100

    tiny changes.

--- src/Stack/primfaktorzerlegung.java
+++ src/Stack/primfaktorzerlegung.java
@@ -1,5 +1,4 @@
 package Stack;
-
 public class primfaktorzerlegung {
     public static intKeller pfz(int x) {
         intKeller k = new intArrayKeller();
@@ -24,8 +23,56 @@ public class primfaktorzerlegung {
         return true;
     }
 
-    public static void main(String[] args)
+    public static void reduziere()
     {
-        System.out.println(pfz(84));
+        intKeller zStapel = pfz(420);
+        intKeller nStapel = pfz(450);
+
+        intKeller tempZStapel = new intArrayKeller();
+        intKeller tempNStapel = new intArrayKeller();
+
+        while (!zStapel.isEmpty()) {
+            tempZStapel.push(zStapel.pop());
+        }
+
+        while (!nStapel.isEmpty()) {
+            tempNStapel.push(nStapel.pop());
+        }
+
+        while (!tempZStapel.isEmpty()) {
+            int aktuell = tempZStapel.pop();
+            boolean gefunden = false;
+
+            while (!tempNStapel.isEmpty()) {
+                nStapel.push(tempNStapel.pop());
+            }
+            while (!nStapel.isEmpty()) {
+                int nCurrent = nStapel.pop();
+                if (nCurrent == aktuell) {
+                    gefunden = true;
+                    break;
+                } else {
+                    tempNStapel.push(nCurrent);
+                }
+            }
+            if (!gefunden) {
+                zStapel.push(aktuell);
+            }
+        }
+        while (!tempNStapel.isEmpty()) {
+            nStapel.push(tempNStapel.pop());
+        }
+    }
+    public static intKeller zStapel = pfz(420);
+    public static intKeller nStapel = pfz(450);
+
+    public static void main(String[] argv) {
+        System.out.println( zStapel );
+        System.out.println( nStapel );
+
+        reduziere();
+
+        System.out.println( zStapel );
+        System.out.println( nStapel );
     }
 }
